5e1ea650d7ecef699b7f9d6c096dad31340bd9b1
chiharu <chiparunn.ishii@gmail.com>
2023-08-29 23:55:30 +0900

    textureの処理とbombの描画を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod bomb;
+mod textures;
+
+use bomb::*;
+use textures::*;
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_systems(PreStartup, setup_window)
+    .add_systems(PreStartup, setup_images)
+    .add_plugins((DefaultPlugins, BombPlugin))
+    .run();
+}
+
+// windowの初期設定
+fn setup_window(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
